38507543d9f7fce3dc180b670d4d83f01fa4f0b8
Jesper Lind <jesper@codeodyssey.se>
2011-10-09 05:48:41 +0200

    Care-fix

--- Services/Care/Care.cs
+++ Services/Care/Care.cs
@@ -11,15 +11,16 @@ namespace FlyttaIn.Services.Care
     {	
         public List<Dictionary<string, string>> GetNearest(float lat, float lon)
         {
-			GeoBox box = GetSurroundingBox(lat.ToString(), lon.ToString(), 2);
-			
-			List<Dictionary<string, string>> list = new List<Dictionary<string, string>>();
-			
+            //GeoBox box = GetSurroundingBox(lat.ToString(), lon.ToString(), 2);
+            
+            List<Dictionary<string, string>> list = new List<Dictionary<string, string>>();
+            
             XmlDocument doc = new XmlDocument();
             doc.Load(HttpContext.Current.ApplicationInstance.Server.MapPath("~/App_Data") + "/vard.xml");
 
             XmlNode root = doc.DocumentElement;
 
+            //Try carpool loop
             foreach(XmlNode pool in root.ChildNodes)
             {
                 var poolList = pool.Cast<XmlNode>().ToList();
@@ -30,16 +31,18 @@ namespace FlyttaIn.Services.Care
 
             return list;
         }
-		
-		public static double distance(float lat1, float lon1, float lat2, float lon2) {
-			double R = 6371;
- 
-	        double dLat = (Math.PI / 180) * (poolLat - lat);
-	        double dLon =  (Math.PI / 180) * (poolLon - lon);
-	 
-	        double a = Math.Sin(dLat / 2) * Math.Sin(dLat / 2) + Math.Cos( (Math.PI / 180) * (poolLat)) *Math.Cos( (Math.PI / 180) * (lat)) * Math.Sin(dLon / 2) * Math.Sin(dLon / 2);
-	        double c = 2 * Math.Asin(Math.Min(1, Math.Sqrt(a)));
-	        double d = Math.Abs(R * c);
-		}
+
+        public static double distance(float lat1, float lon1, float lat2, float lon2) {
+            double R = 6371;
+
+            double dLat = (Math.PI / 180) * (lat1 - lat2);
+            double dLon =  (Math.PI / 180) * (lon1 - lon2);
+     
+            double a = Math.Sin(dLat / 2) * Math.Sin(dLat / 2) + Math.Cos( (Math.PI / 180) * (lat1)) *Math.Cos( (Math.PI / 180) * (lat2)) * Math.Sin(dLon / 2) * Math.Sin(dLon / 2);
+            double c = 2 * Math.Asin(Math.Min(1, Math.Sqrt(a)));
+            double d = Math.Abs(R * c);
+
+            return d;
+        }
     }
 }
