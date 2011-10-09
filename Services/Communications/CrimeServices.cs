0934d00a771b7ae3f659ed7ec8fd4751a119b367
Jesper Lind <jesper@codeodyssey.se>
2011-10-09 02:28:34 +0200

    crime fix

--- Services/Communications/CrimeServices.cs
+++ Services/Communications/CrimeServices.cs
@@ -11,19 +11,19 @@ namespace FlyttaIn.Services.Communications
     {
         public const string CRIMEURL = "http://brottsplatskartan.se/api.php?action=getEvents&period={0}&area={1}";
 
-        public List<CrimeEvent> GetCrimes(int hoursPassed, string lan)
+        public List<CrimeEvent> GetCrimes(int minutesPassed, string lan)
         {
             List<CrimeEvent> retval = new List<CrimeEvent>();
-            var newUrl = string.Format(CRIMEURL, hoursPassed.ToString(), lan);
+            var newUrl = string.Format(CRIMEURL, minutesPassed.ToString(), lan);
             var res = Helper.CreateHttpGet(newUrl, Helper.ContentType.XML);
             XmlDocument doc = new XmlDocument();
             doc.LoadXml(res);
             var node = doc.ChildNodes[0];
             var root = doc.DocumentElement;
-            var events = root.SelectNodes("Event");
+            var events = root.SelectNodes("event");
             foreach (XmlNode eventObject in events)
             {
-                var eventChildren = events.Cast<XmlNode>();
+                var eventChildren = eventObject.Cast<XmlNode>();
                 var title = eventChildren.Where(x => x.Name == "title").Single();
                 var link = eventChildren.Where(x => x.Name == "link").Single();
                 var description = eventChildren.Where(x => x.Name == "description").Single();
