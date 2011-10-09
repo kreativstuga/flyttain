08bb239014fc7e69872d8f3f98e7a3d13debf0bf
unknown <Lillen@.(none)>
2011-10-09 06:10:55 +0200

    added eniro

--- Services/Communications/GoteborgServices.cs
+++ Services/Communications/GoteborgServices.cs
@@ -10,7 +10,10 @@ namespace FlyttaIn.Services.Communications
 {
     public class GoteborgServices
     {
-
+        public const string COMMUTER_PARKINGS = "http://data.goteborg.se/ParkingService/v1.1/CommuterParkings/{0}?latitude={1}&longitude={2}&radius={3}&format=Json";
+        public const string HANDICAP_PARKINGS = "http://data.goteborg.se/ParkingService/v1.1/HandicapParkings/{0}?latitude={1}&longitude={2}&radius={3}&format=Json";
+        public const string GBG_BIKE_SERVICE_STATIONS = "http://data.goteborg.se/CykelService/v0.1/GetServiceStations/{0}?format=Json";
+        
         private const string API_KEY = "69aeda59-f7a9-4667-81fb-c6ad913f8eb9";
 
         public const string VASTTRAFIK_NAME_TO_COORD = "http://api.vasttrafik.se/bin/rest.exe/v1/location.name?authKey={0}&format=json&jsonpCallback=processJSON&input={1}";
@@ -19,6 +22,29 @@ namespace FlyttaIn.Services.Communications
         public const string VASTTRAFIK_TRIP = "http://api.vasttrafik.se/bin/rest.exe/v1/trip?authKey={0}&format=json&jsonpCallback=processJSON&date={1}&time={2}&originId={3}&originCoordLat={4}&originCoordLong={5}&destId={6}&destCoordLat={7}&destCoordLong={8}";
         public const string VASTTRAFIK_API_KEY = "e39042b8-db1a-46a9-ba81-2ae8893aa393";
 
+
+        #region Commuter Parkings
+        public string GetParkingCommuter(string lat, string lng, string radius)
+        {
+            var newUrl = string.Format(COMMUTER_PARKINGS, API_KEY, lat, lng, radius);
+            return Helper.CreateHttpGet(newUrl, Helper.ContentType.Json);
+        }
+
+        public string GetParkingHandicap(string lat, string lng, string radius)
+        {
+            var newUrl = string.Format(HANDICAP_PARKINGS, API_KEY, lat, lng, radius);
+            return Helper.CreateHttpGet( newUrl, Helper.ContentType.Json);
+        }
+        #endregion
+
+        #region Cykelservice - fungerar
+        public string GetBikeServiceStations()
+        {
+            var newUrl = string.Format(GBG_BIKE_SERVICE_STATIONS, API_KEY);
+            var res = Helper.CreateHttpGet(newUrl, Helper.ContentType.Json);
+            return res;
+        }
+        #endregion
 
         /// <summary>
         /// call function GetGBGLocationToCoord in this class to get identifier and coordinates
